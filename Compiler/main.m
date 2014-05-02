e21278c139c4fab8f516fdf7a0d3eb14ca9b2f95
Freddy Kellison-Linn <fred.kl@me.com>
2014-05-01 17:44:49 -0700

    Initial Commit

---
+++ Compiler/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Compiler
+//
+//  Created by Freddy Kellison-Linn on 5/1/14.
+//  Copyright (c) 2014 Jumhyn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
